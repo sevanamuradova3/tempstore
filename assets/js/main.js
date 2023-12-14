41b76b40af7fdde382e0533e1fc77380e4f8599c
sevanamuradova3 <sd70rs4xn@code.edu.az>
2023-12-14 11:58:02 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const customers = document.getElementById("customers");
+const cartButton = document.querySelector('.cartButton')
+
+axios.get('https://dummyjson.com/products')
+.then(res => {
+    db = res.data.products;
+    console.log(db);
+    db.map((item) => {
+        let card = document.createElement('div');
+        card.className = "cardBox";
+        card.innerHTML = `
+            <img src="${item.thumbnail}" alt="">
+            <div class="cardTextBox">
+                <h6>${item.title}</h6>
+                <p>${item.price} $</p>
+            </div>
+            <button onclick="addToCart(${item.id})">  <i class="fa-solid fa-heart"></i></button>
+        `;
+        customers.appendChild(card);
+    });
+});
+
+function addToCart(productIndex) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(db.find(item => item.id === productIndex));
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCartCount()
+}
+
+function displayCartCount () {
+    let cart = JSON.parse(localStorage.getItem('cart')) || []
+    cartButton.innerHTML = `<i class="fa-solid fa-cart-shopping" style="font-size: 20px;"></i><p class="cartCount">${cart.length}</p>`
+}
+
+window.onload = () => {
+    displayCartCount()
+}
